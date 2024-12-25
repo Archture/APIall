7dd4f2763b7a386b9531c4752eeef059d71225f9
Archture <50861315+Archture@users.noreply.github.com>
2024-12-25 09:25:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from azure.core.credentials import AzureKeyCredential
 from fastapi import FastAPI, Request, Header
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 
 app = FastAPI()
 
@@ -25,15 +26,14 @@ class Message(BaseModel):
     ken: str
     sys: str = "You are a helpful assistant."
 
-
-async def OpenAIf(msg)
+async def OpenAIf(msg):
     endpoint = "https://models.inference.ai.azure.com"
-    
+
     client = ChatCompletionsClient(
         endpoint=endpoint,
         credential=AzureKeyCredential(msg.kenOpenAi),
     )
-    
+
     response = client.complete(
         messages=[
             SystemMessage(content=msg.sys),
@@ -49,58 +49,56 @@ async def OpenAIf(msg)
 
 async def Geminif(msg):
     genai.configure(api_key=msg.kenGemini)
-  
+
     model = genai.GenerativeModel(msg.modelGemini)
     response = model.generate_content(msg.messageGemini)
-    
+
     Response = response.text
     return Response
 
-async def Requestf(url, msg):
+async def Requestf(msg):
     urls = {
-    "https://api.x.ai/v1/chat/completions": ["Bearer "+msg.kenX, "grok-beta"],
-    "https://api.groq.com/openai/v1/chat/completions": ["Bearer "msg.kenGroq,"llama-3.3-70b-specdec"], 
-    "https://api.mistral.ai/v1/chat/completions":["Bearer "msg.kenMistral, "mistral-large-latest"],
+        "https://api.x.ai/v1/chat/completions": ["Bearer "+msg.kenX, "grok-beta"],
+        "https://api.groq.com/openai/v1/chat/completions": ["Bearer "+msg.kenGroq, "llama-3.3-70b-specdec"],
+        "https://api.mistral.ai/v1/chat/completions": ["Bearer "+msg.kenMistral, "mistral-large-latest"],
     }
-    
+
     for url, API in urls.items():
-    
         headers = {
             "Content-Type": "application/json",
             "Authorization": API[0]
         }
-        
+
         data = {
-            # "model": ,
             "model": API[1],
             "messages": [{"role": "user","content": msg.messageRequest}]
         }
-    
+
         response = requests.post(url, headers=headers, json=data, timeout=500)
-    
+
         if response.status_code == 200:
-            return response.json()['choices'][0]['message']['content'])
+            return response.json()['choices'][0]['message']['content']
 
 @app.post("/message")
 async def receive_message(msg: Message):
     ResponseOpenAI = ''
     ResponseGemini = ''
     ResponseRequest = ''
-    
+
     try:
-        ResponseOpenAI = OpenAIf(msg)
+        ResponseOpenAI = await OpenAIf(msg)
     except:
         pass
     try:
-        ResponseGemini = Geminif(msg)
+        ResponseGemini = await Geminif(msg)
     except:
         pass
     try:
-        ResponseRequest = Requestf(msg)
+        ResponseRequest = await Requestf(msg)
     except:
         pass
 
-    return ResponseOpenAI+ResponseGemini+ResponseRequest
+    return ResponseOpenAI + ResponseGemini + ResponseRequest
 
 @app.get("/")
 async def root():
