4c3d2b813a1549c9215ba789158a27297ba117b2
Archture <50861315+Archture@users.noreply.github.com>
2025-02-11 12:29:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,10 +51,9 @@ class Message(BaseModel):
     kenBaiduSec: str
     kenTogether: str
     kenOpenRouter: str
-
-
     
     sys: str = "You are a helpful assistant."
+    sentence: str
 
 def get_access_token(msg: Message):
     url = "https://aip.baidubce.com/oauth/2.0/token"
@@ -81,7 +80,7 @@ def ask_Q(msg: Message):
         f"wenxinworkshop/chat/ernie-speed-128k?access_token={access_token}"
     )
     payload = json.dumps({
-        "messages": [{"role": "user", "content": msg.messageBaidu}]
+        "messages": [{"role": "user", "content": msg.messageBaidu + msg.sentence}]
     })
     headers = {"Content-Type": "application/json"}
 
@@ -98,7 +97,7 @@ async def OpenAIf(msg: Message):
     client = ChatCompletionsClient(endpoint=endpoint, credential=AzureKeyCredential(msg.kenOpenAi))
     response = await asyncio.to_thread(
         client.complete,
-        messages=[SystemMessage(content=msg.sys), UserMessage(content=msg.messageOpenAi)],
+        messages=[SystemMessage(content=msg.sys), UserMessage(content=msg.messageOpenAi + msg.sentence)],
         temperature=1.0,
         top_p=1.0,
         model=msg.modelOpenAi
@@ -108,7 +107,7 @@ async def OpenAIf(msg: Message):
 async def Geminif(msg: Message):
     genai.configure(api_key=msg.kenGemini)
     model = genai.GenerativeModel(msg.modelGemini)
-    response = await asyncio.to_thread(model.generate_content, msg.messageGemini)
+    response = await asyncio.to_thread(model.generate_content, msg.messageGemini + msg.sentence)
     print('Gemini: '+ response)
     return response.text
 
@@ -123,13 +122,13 @@ async def fetch_async(session: aiohttp.ClientSession, url: str, headers: dict, d
 
 async def Requestf(msg: Message):
     urls = {
-        "https://open.bigmodel.cn/api/paas/v4/chat/completions": ["Bearer " + msg.kenX, msg.modelX, msg.messageX],
-        "https://api.groq.com/openai/v1/chat/completions": ["Bearer " + msg.kenGroq, msg.modelGroq, msg.messageGroq],
-        "https://api.mistral.ai/v1/chat/completions": ["Bearer " + msg.kenMistral, msg.modelMistral, msg.messageMistral],
-        "https://api.together.xyz/v1/chat/completions":["Bearer " + msg.kenTogether, msg.modelTogether, msg.messageTogether],
-        "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions":["Bearer " + msg.kenGemini, msg.modelGemini, msg.messageGemini],
-        "https://models.inference.ai.azure.com/chat/completions":["Bearer " + msg.kenOpenAi, msg.modelOpenAi, msg.messageOpenAi]，
-        "https://openrouter.ai/api/v1/chat/completions":["Bearer " + msg.kenOpenRouter, msg.modelOpenRouter, msg.messageOpenRouter]，
+        "https://open.bigmodel.cn/api/paas/v4/chat/completions": ["Bearer " + msg.kenX, msg.modelX, msg.messageX + msg.sentence],
+        "https://api.groq.com/openai/v1/chat/completions": ["Bearer " + msg.kenGroq, msg.modelGroq, msg.messageGroq + msg.sentence],
+        "https://api.mistral.ai/v1/chat/completions": ["Bearer " + msg.kenMistral, msg.modelMistral, msg.messageMistral + msg.sentence],
+        "https://api.together.xyz/v1/chat/completions":["Bearer " + msg.kenTogether, msg.modelTogether, msg.messageTogether + msg.sentence],
+        "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions":["Bearer " + msg.kenGemini, msg.modelGemini, msg.messageGemini + msg.sentence],
+        "https://models.inference.ai.azure.com/chat/completions":["Bearer " + msg.kenOpenAi, msg.modelOpenAi, msg.messageOpenAi + msg.sentence]，
+        "https://openrouter.ai/api/v1/chat/completions":["Bearer " + msg.kenOpenRouter, msg.modelOpenRouter, msg.messageOpenRouter + msg.sentence]，
 
     }
 
@@ -156,7 +155,7 @@ async def RequestfAlt(msg: Message):
     }
     data = {
         "model": msg.modelCohere,
-        "messages": [{"role": "user", "content": msg.messageCohere}]
+        "messages": [{"role": "user", "content": msg.messageCohere + msg.sentence}]
     }
     
     try:
