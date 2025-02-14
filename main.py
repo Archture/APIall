794cdfd04ce9d897970f671d1329426956f8e7f7
Archture <50861315+Archture@users.noreply.github.com>
2025-02-14 13:38:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class Message(BaseModel):
     messageTogether: str
     messageOpenRouter: str 
     messageCF: str
-    messageOVH: str = "a flirtatious and spicy tone:"
+    messageOVH: str = "humorous joke:"
     messageChutes: str = "a flirtatious and spicy tone:"
     messageTargon: str = "a flirtatious and spicy tone:"
 
@@ -194,7 +194,7 @@ async def Geminif(msg: Message):
 
 async def fetch_async(session: aiohttp.ClientSession, url: str, headers: dict, data: dict) -> str:
     try:
-        async with session.post(url, headers=headers, json=data, timeout=5) as r:
+        async with session.post(url, headers=headers, json=data, timeout=30) as r:
             r_json = await r.json()
             return r_json['choices'][0]['message']['content']
     except Exception as e:
