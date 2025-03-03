f81024237548d1e70e93845f56251ae9f69a631a
Archture <50861315+Archture@users.noreply.github.com>
2025-03-03 15:38:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-timeout = 16
+timeout = 14
 
 # -------------------- FastAPI Setup --------------------
 app = FastAPI()
