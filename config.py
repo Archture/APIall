4aaf09f07c692f0d6b84bad8bd89b9b1dbb4cff6
Archture <50861315+Archture@users.noreply.github.com>
2026-02-10 03:02:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -35,4 +35,7 @@ class Settings:
     ZENMUX_API_KEY = os.getenv("ZENMUX_API_KEY")
     LONGCAT_API_KEY = os.getenv("LONGCAT_API_KEY")
     NVIDIA_API_KEY = os.getenv("NVIDIA_API_KEY")
+    LITEROUTER_API_KEY = os.getenv("LITEROUTER_API_KEY")
+    ROUTEWAY_API_KEY = os.getenv("ROUTEWAY_API_KEY")
+    POIXE_API_KEY = os.getenv("POIXE_API_KEY")
 settings = Settings()
